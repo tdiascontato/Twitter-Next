059373d0d6fe8d329a9799be849ac90c1a7f2798
tiago dias <tdiacontato@gmail.com>
2023-11-27 18:46:39 -0300

    third  commit

--- components/layout/SidebarTweetButton.tsx
+++ components/layout/SidebarTweetButton.tsx
@@ -1,14 +1,31 @@
-import { useRouter } from "next/router";
+// import { useRouter } from "next/router";
+import { useCallback } from "react";
+import useLoginModal from "@/hooks/useLoginModal";
 import { FaFeather } from "react-icons/fa";
+
 const SidebarTweetButton = () => {
-    const router = useRouter();
+    
+    // const router = useRouter();
+    // ()=>router.push('/')
+    const loginModal = useLoginModal();
+
+    const onClick = useCallback(()=>{
+        loginModal.onOpen();
+    }, [loginModal]);
+
     return (
-    <div onClick={()=>router.push('/')}>
-        <div className="mt-6 lg:hidden rounded-full h-14 w-14 p-4 flex items-center justify-center bg-sky-500 hover:bg-opacity-80 transition cursor-pointer">
+    <div onClick={onClick}>
+        <div className="mt-6 lg:hidden rounded-full 
+        h-14 w-14 p-4 flex items-center justify-center 
+        bg-sky-500 hover:bg-opacity-80 transition 
+        cursor-pointer">
             <FaFeather size={24} color="white"/>
         </div>
-        <div className="mt-6 hidden lg:block px-4 py-2 rounded-full bg-sky-500 hover:bg-opacity-90 cursor-pointer transition">
-            <p className="hidden lg:block text-center font-semibold text-white text-[20px]">
+        <div className="mt-6 hidden lg:block px-4 py-2 
+        rounded-full bg-sky-500 hover:bg-opacity-90 
+        cursor-pointer transition">
+            <p className="hidden lg:block text-center 
+            font-semibold text-white text-[20px]">
                 Tweet
             </p>
         </div>
---
+++ components/modals/LoginModal.tsx
@@ -0,0 +1,80 @@
+import { useCallback, useState} from "react";
+import useLoginModal from "@/hooks/useLoginModal"; 
+import useRegisterModal from "@/hooks/useRegisterModal";
+import Input from "../Input";
+import Modal from "../Modal";
+
+const LoginModal = () => {
+    const loginModal = useLoginModal();
+    const registerModal = useRegisterModal();
+    
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState(''); 
+    const [isLoading, setIsLoading] = useState(false);
+
+    const onToggle = useCallback(()=>{
+        if(isLoading){
+            return;
+        }
+        loginModal.onClose();
+        registerModal.onOpen();
+    }, [isLoading, registerModal, loginModal])
+
+    const onSubmit = useCallback(async () => { 
+        try{
+        setIsLoading(true);
+        // TODO ADD LOG IN
+        loginModal.onClose();
+    } catch (error) { 
+        console.log(error); 
+    } finally {
+        setIsLoading(false);
+    }
+    }, [loginModal]);
+
+    const bodyContent = (
+        <div className="flex flex-col gap-4">
+            <Input
+            placeholder="Email" 
+            onChange={(e)=> setEmail(e.target.value)} 
+            value = {email}
+            disabled = {isLoading}
+            />
+            <Input
+            placeholder="Senha" 
+            onChange={(e)=>setPassword(e.target.value)} 
+            value = {password}
+            disabled = {isLoading}
+            />
+
+        </div>
+    )
+
+    const footerContent = (
+        <div className="text-neutal-400 text-center mt-4">
+            <p>Primeira vez aqui?
+            <span
+                onClick={onToggle} 
+                className="
+                text-white cursor-pointer 
+                hover:underline">
+                Criar conta!
+            </span>
+            </p>
+        </div>
+    )
+
+    return (
+    <Modal 
+        disabled = {isLoading}
+        isOpen = {loginModal.isOpen}
+        title = "Login"
+        actionLabel = "Entrar"
+        onClose = {loginModal.onClose}
+        onSubmit = {onSubmit}
+        body={bodyContent}
+        footer={footerContent}
+    />
+    );
+}
+export default LoginModal;
